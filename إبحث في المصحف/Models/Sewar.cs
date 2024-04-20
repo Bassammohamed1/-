e9b22a4d8da1b0d9257901dd212a1798a8030349
Bassammohamed1 <Bassammohamed1009@gmail.com>
2024-04-20 20:11:49 +0200

    Commited

--- إبحث في المصحف/Models/Sewar.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace إبحث_في_المصحف.Models
-{
-    public class Sewar
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Content  { get; set; }
-    }
-}
